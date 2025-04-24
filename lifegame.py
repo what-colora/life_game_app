160298fb8167147a43fd61549f142b622969556d
what-colora <kousei_ohtani@dtc.co.jp>
2025-04-24 11:37:47 +0900

    課題用にロジックの実装部分の分離

--- lifegame.py
+++ lifegame.py
@@ -1,24 +1,28 @@
-def next_step(grid):
-    # グリッドは たて:よこ = 30:30マス
+
+def next_step(grid):    
+    # グリッドのサイズ たて:よこ = 30:30マス
     size = 30
-    # 1ステップ進めた後の、グリッド情報保存用の2次元配列。結果格納用
+    # 結果格納用の2次元配列
     result = [[0 for _ in range(size)] for _ in range(size)]
 
-    # グリッドを走査する2重ループ。
-    # iは行（たて）、jは列（よこ）方向
+    # 現在地(grid[i][j])の周囲8セルに存在する生存セル(1)の個数をカウントする関数
+    def count_neighbors(i,j):
+        res = 0
+        res = sum(grid[x][y] for x in range(max(0, i-1), min(size, i+2))
+                    for y in range(max(0, j-1), min(size, j+2)) if (x, y) != (i, j))
+
+        return res
+
+    # 次ステップのグリッドの描画・計算処理
     for i in range(size):
         for j in range(size):
-            # 自身の周囲8セルの走査(下図で×からみて○が対象)
-            #   ○○○
-            #   ○×○
-            #   ○○○
-            # 周囲8セルにおいて生存セルの合計数を変数neighborsに格納
-            neighbors = sum(grid[x][y] for x in range(max(0, i-1), min(size, i+2))
-                           for y in range(max(0, j-1), min(size, j+2)) if (x, y) != (i, j))
-            # 自身が生存セルの場合の処理
+            # 現在地(grid[i][j])の周囲8セルにある生存セルの個数
+            neighbors = count_neighbors(i,j)
+        
+            # 現在地(grid[i][j])が生存セル(1)の場合の処理
             if grid[i][j] == 1:
-                result[i][j] = 1 if neighbors in [2, 3] else 0
-            # 自身が死亡セルの場合の処理
+                result[i][j] = 1 if neighbors in [2, 3] else 0    
+            # 現在地(grid[i][j])が死亡セル(0)の場合の処理
             else:
                 result[i][j] = 1 if neighbors == 3 else 0
 
@@ -26,3 +30,4 @@ def next_step(grid):
 
 if __name__ == "__main__":
     next_step()
+
